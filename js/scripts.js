a1b0cdd5668ca3a32ba359f76be9813227076c45
Alexander <knibz@protonmail.com>
2022-03-29 23:19:47 -0700

    add script p to file

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 jQuery("h1").click(function() {
   alert("This is a heading.");
 });
+
+jQuery("p").click(function(){
+  alert("This is a paragrah.");
+});
+
+jQuery("img").click(function() {
+  alert("This is an image.");
+});
